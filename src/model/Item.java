e6efe1b699fcb1102dbff160e12e094556ec838c
luisfrojas <35510878+luisfrojas@users.noreply.github.com>
2018-04-12 17:16:10 -0500

    Propuesta del modelo para almacenar los resultados que se obtengan de la red neuronal.

---
+++ src/model/Item.java
@@ -0,0 +1,18 @@
+package model;
+
+public class Item {
+
+    /**
+     * variablesX, pesosW y incrementoW tienen la misma longitud, a cada variable le corresponde un peso
+     */
+
+    private int[] variablesX;
+    private int YDeseada;
+    private double[] pesosW;
+    private double sumatoria;
+    private double funcionEntrada;
+    private double funcionActivacion;
+    private double error;
+    private double[] incrementoW;
+
+}
