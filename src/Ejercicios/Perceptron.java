b732d37ca20bb518d34e4e68168ea5ded39b1a19
luisfrojas <35510878+luisfrojas@users.noreply.github.com>
2018-04-12 19:52:34 -0500

    Updates

--- src/Ejercicios/Perceptron.java
+++ src/Ejercicios/Perceptron.java
@@ -7,29 +7,24 @@ import java.text.DecimalFormat;
 public class Perceptron {
 
     public static double calculoSumatoria(Modelo item, double limite) {
-        double result = 0.0;
+        double resultado = 0.0;
 
         int[] variables = item.getVariablesX();
         double[] pesos = item.getPesosW();
 
-        double sum = 0.0;
+        double sumatoria = 0.0;
         for (int i=0; i<variables.length; i++) {
-
-            double aux = variables[i] * pesos[i];
-            sum = sum + aux;
+            sumatoria += variables[i] * pesos[i];
         }
 
         DecimalFormat df = new DecimalFormat("#.#");
+        resultado = sumatoria - limite;
+        System.out.println("Resultado: " + df.format(resultado));
 
-        System.out.println("SUM: " + df.format(sum));
-        System.out.println("LIM: " + df.format(limite));
-        result = sum - limite;
-
-        System.out.println("Result: " + df.format(result));
-        return result;
+        return resultado;
     }
 
-    public static void calcularFuncionActivacion(Modelo item,double funcionEntrada){
+    public static void calcularFuncionActivacion(Modelo item, double funcionEntrada){
         if(funcionEntrada>0){
             item.setFuncionActivacion(1);
         }else{
@@ -37,6 +32,5 @@ public class Perceptron {
             item.setFuncionActivacion(0);
         }
     }
-
 
 }
