11d22fdac6d7b7d081dbc21d7950320805e6f71e
luisfrojas <35510878+luisfrojas@users.noreply.github.com>
2018-04-16 18:15:59 -0500

    Updates

--- src/MainHopfield.java
+++ src/MainHopfield.java
@@ -2,32 +2,64 @@ import metodo.Hopfield;
 
 public class MainHopfield {
 
-    private static int[] P1 = new int[]{
-            -1, -1, 1, -1,
-            -1, -1, 1, -1,
-            -1, -1, 1, -1,
-            -1, -1, 1, -1
-    };
-
-    private static int[] P2 = new int[]{
-            1, 1, 1, 1,
-            1, -1, -1, 1,
-            1, -1, -1, 1,
-            1, 1, 1, 1
-    };
+    private static int[][] P1 = new int[4][4];
+    private static int[][] P2 = new int[4][4];
 
     public static void main(String[] args) {
+        cargaPatrones();
+        Hopfield.calcularMatrizEntrenamiento(P1);
+    }
 
-        int[][] matrizEntrenamientoP1 = Hopfield.calcularMatrizEntrenamiento(P1);
+    private static void cargaPatrones() {
 
-        String row = "";
-        for (int x=0; x<matrizEntrenamientoP1.length; x++) {
-            for (int y=0; y<matrizEntrenamientoP1[x].length; y++) {
-                row += matrizEntrenamientoP1[x][y] + " ";
-            }
-            row += "\n";
-        }
+        /*  -1  -1  1  -1
+            -1  -1  1  -1
+            -1  -1  1  -1
+            -1  -1  1  -1  */
 
-        System.out.println(row);
+        P1[0][0] = -1;
+        P1[0][1] = -1;
+        P1[0][2] = 1;
+        P1[0][3] = -1;
+
+        P1[1][0] = -1;
+        P1[1][1] = -1;
+        P1[1][2] = 1;
+        P1[1][3] = -1;
+
+        P1[2][0] = -1;
+        P1[2][1] = -1;
+        P1[2][2] = 1;
+        P1[2][3] = -1;
+
+        P1[3][0] = -1;
+        P1[3][1] = -1;
+        P1[3][2] = 1;
+        P1[3][3] = -1;
+
+        /*  1  1  1  1
+            1 -1 -1  1
+            1 -1 -1  1
+            1  1  1  1  */
+
+        P2[0][0] = 1;
+        P2[0][1] = 1;
+        P2[0][2] = 1;
+        P2[0][3] = 1;
+
+        P2[1][0] = 1;
+        P2[1][1] = -1;
+        P2[1][2] = -1;
+        P2[1][3] = 1;
+
+        P2[2][0] = 1;
+        P2[2][1] = -1;
+        P2[2][2] = -1;
+        P2[2][3] = 1;
+
+        P2[3][0] = 1;
+        P2[3][1] = 1;
+        P2[3][2] = 1;
+        P2[3][3] = 1;
     }
 }
--- src/metodo/Hopfield.java
+++ src/metodo/Hopfield.java
@@ -3,21 +3,92 @@ package metodo;
 public class Hopfield {
 
     /**
-     * Calcula la matriz por su matriz transpuesta menos la matriz identidad
-     * MxM(T) - I
+     *  Calcula la matriz por su matriz transpuesta menos la matriz identidad
+     *  Matriz * MatrizTranspuesta - Matriz Identidad
      */
 
-    public static int[][] calcularMatrizEntrenamiento(int[] matriz) {
+    public static int[][] calcularMatrizEntrenamiento(int[][] matriz) {
 
-        int[] matrizT = matriz; // Matriz Transpuesta (solo de una dimension)
-        int[][] matrizResultante = new int[matriz.length][matrizT.length];
+        int[][] matrizT = calcularMatrizTranspuesta(matriz);
+        int[][] matrizResultante = multiplicarMatrices(matriz, matrizT);
 
-        for (int x=0; x<matriz.length; x++) {
-            for (int y=0; y<matrizT.length; y++) {
-                matrizResultante[x][y] = matriz[x] * matriz[y];
+        int[][] matrizIdentidad = calcularMatrizIdentidad(matriz);
+
+        imprimeMatriz(matrizIdentidad);
+
+        return matrizResultante;
+    }
+
+    private static int[][] calcularMatrizTranspuesta(int[][] matriz) {
+        int[][] matrizT = new int[matriz[0].length][matriz.length];
+
+        for (int x=0; x < matriz.length; x++) {
+            for (int y=0; y < matriz[x].length; y++) {
+                matrizT[y][x] = matriz[x][y];
             }
         }
 
-        return matrizResultante;
+        return matrizT;
+    }
+
+    private static int[][] multiplicarMatrices(int[][] m1, int[][] m2) {
+
+        int fil_m1 = m1.length;
+        int col_m1 = m1[0].length;
+
+        int fil_m2 = m2.length;
+        int col_m2 = m2[0].length;
+
+        /*  Solo puede multiplicar dos matrices si sus dimensiones son compatibles,
+            lo que significa que el número de columnas en la primera matriz es igual
+            al número de renglones en la segunda matriz.
+            Si A es una matriz a × b y B es una matriz b × c , el producto AB es una matriz a × c.  */
+
+        if (col_m1 != fil_m2)
+            throw new RuntimeException("No se pueden multiplicar las matrices");
+
+        int[][] multiplicacion = new int[fil_m1][col_m2];
+
+        for (int x=0; x<multiplicacion.length; x++) {
+            for (int y=0; y<multiplicacion[x].length; y++) {
+                for (int z=0; z<col_m1; z++) {
+                    multiplicacion [x][y] += m1[x][z] * m2[z][y];
+                }
+            }
+        }
+
+        return multiplicacion;
+    }
+
+    private static int[][] calcularMatrizIdentidad(int[][] matriz) {
+
+        int filas = matriz.length;
+        int columnas = matriz[0].length;
+
+        int[][] matrizIdentidad = new int[filas][columnas];
+
+        for (int x=0; x < filas; x++) {
+            for (int y=0; y < columnas; y++) {
+                if (x == y) {
+                    matrizIdentidad[x][y] = 1;
+                } else {
+                    matrizIdentidad[x][y] = 0;
+                }
+            }
+        }
+
+        return matrizIdentidad;
+    }
+
+    private static void imprimeMatriz(int[][] matriz) {
+        String strMatriz = "";
+        for (int x=0; x<matriz.length; x++) {
+            for (int y=0; y<matriz[x].length; y++) {
+                strMatriz += matriz[x][y] + " ";
+            }
+            strMatriz += "\n";
+        }
+
+        System.out.println(strMatriz);
     }
 }
