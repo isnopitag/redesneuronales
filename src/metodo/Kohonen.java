d5b3acb3b7032bfcf28c4a139fd50f51f24fcb42
Carmona <13030617@itcelaya.edu.mx>
2018-04-19 23:21:21 -0500

    Eliminacion de metodos y cositas

--- src/metodo/Kohonen.java
+++ src/metodo/Kohonen.java
@@ -8,15 +8,13 @@ public class Kohonen {
                 double d[] = new double[matrizPesos.length];
                 for (int j = 0; j < matrizPesos[0].length ; j++) {
                     for (int k = 0; k < matrizPesos.length ; k++) {
-                        //System.out.println(matrizEntrada[i][j]+" "+matrizPesos[k][j]);
                         d[k]+= Math.pow((matrizEntrada[i][j] - matrizPesos[k][j]), 2);
                     }
-
                 }
-                //System.out.println(min(d));
-                matrizPesos=calcularNuevaMatrizDePesos(matrizEntrada,matrizPesos,min(d),i,alfa);
-
+                matrizPesos=calcularNuevaMatrizDePesos(matrizEntrada,matrizPesos,indiceMenor(d),i,alfa);
             }
+            //Solo para debugear que este vien la matriz de pesos
+            imprimeMatriz(matrizPesos);
 
         }
 
@@ -26,36 +24,14 @@ public class Kohonen {
                 matrizPesos[index][i] = matrizPesos[index][i]+(alfa*(matrizEntrada[di][i]-matrizPesos[index][i]));
                 System.out.println(matrizPesos[index][i]);
             }
-            //System.out.println("\n");
+            System.out.println("\n");
             return matrizPesos;
         }
 
-    public static void imprimeArrayDouble(double[] arrayDistancia) {
-        String strMatriz = "";
-        for (int x=0; x<arrayDistancia.length; x++) {
-                strMatriz += arrayDistancia[x]+ " ";
-            strMatriz += "\n";
-        }
-        System.out.println(strMatriz);
-    }
-
-    public static int obtenerMenorDistancia(double[] arrayDistancia){
-            double menor=0.0;
-            int    index=0;
-        for (int i = 1; i < arrayDistancia.length; i++) {
-            if(menor>arrayDistancia[i]){
-                menor=arrayDistancia[i];
-                index=i;
-            }
-        }
-        return index;
-    }
-
-    public static int min(double[] numbers) {
-        if (numbers == null || numbers.length == 0) return -1; // Saves time for empty array
-        // As pointed out by ZouZou, you can save an iteration by assuming the first index is the smallest
-        double minVal = numbers[0]; // Keeps a running count of the smallest value so far
-        int minIdx = 0; // Will store the index of minVal
+    public static int indiceMenor(double[] numbers) {
+        if (numbers == null || numbers.length == 0) return -1;
+        double minVal = numbers[0];
+        int minIdx = 0;
         for(int idx=1; idx<numbers.length; idx++) {
             if(numbers[idx] <= minVal) {
                 minVal = numbers[idx];
@@ -65,6 +41,16 @@ public class Kohonen {
         return minIdx;
     }
 
+    public static void imprimeMatriz(double[][] matriz) {
+        String strMatriz = "";
+        for (int x=0; x<matriz.length; x++) {
+            for (int y=0; y<matriz[x].length; y++) {
+                strMatriz += matriz[x][y] + " ";
+            }
+            strMatriz += "\n";
+        }
+        System.out.println(strMatriz);
+    }
 
 }
 
